68035d429787930ff144a9454ec6c0d856529772
ubar-on <ubaron@gmail.com>
2021-05-30 15:25:35 +0000

    Refactored Palindrome.

--- lib/ubaron_palindrome.rb
+++ lib/ubaron_palindrome.rb
@@ -2,7 +2,18 @@
 
 require_relative "ubaron_palindrome/version"
 
-module UbaronPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
+    
 end
--- test/ubaron_palindrome_test.rb
+++ test/ubaron_palindrome_test.rb
@@ -3,11 +3,19 @@
 require "test_helper"
 
 class UbaronPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::UbaronPalindrome::VERSION
+  def test_non_palindrome
+    refute "apple".palindrome?
+  end
+  
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+  
+  def test_mixed_case_palindrome
+    assert "Racecar".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert true
-  end
+  def test_palindrome_with_punctuation
+      assert "Madam, I'm Adam.".palindrome?
+  end  
 end
